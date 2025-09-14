7c5d0098eb57d1c7ed12f80256eec821e5bed97b
Josué <rnascimentojosue@gmail.com>
2025-09-13 23:31:33 -0300

    add standart

--- src/App.tsx
+++ src/App.tsx
@@ -99,7 +99,7 @@ function App() {
         <div className="flex items-center justify-center space-x-2">
           <Clock className="w-5 h-5" />
           <span>
-            ATTENTION ! Cette présentation sera disponible seulement jusqu’au:{" "}
+            ATTENTION ! Cette présentation sera disponible seulement jusqu’ au:{" "}
             {""}
             {formattedDate}
           </span>
